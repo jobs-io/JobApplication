98d57d94a1c7552f00d61932a7fa7c3872cac41c
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2021-07-19 22:09:12 +1200

    remove redundant code

--- src/JobApplication/Data/IDataStore.cs
+++ src/JobApplication/Data/IDataStore.cs
@@ -7,9 +7,6 @@ namespace JobApplication.Data
         IDictionary<string, string> GetJobApplication();
         void CreateJobApplication(string source, IDictionary<string, string> job);
         bool Exists(string source);
-
-        void UpdateCv(string source, string cv);
-        void UpdateCoverLetter(string source, string coverLetter);
         void Update<T>(string source, string key, T data);
     }
 }
